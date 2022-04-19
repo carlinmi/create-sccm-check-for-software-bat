e15de6a688f92a71a21a465d2e6f2561dec96ada
Carlin Mitchell <carlinmi@umn.edu>
2022-04-19 15:09:47 -0500

    the rest of the owl

--- create-sccm-check-for-software-bat.py
+++ create-sccm-check-for-software-bat.py
@@ -1,2 +1,22 @@
 import subprocess
+import re
+
+# find the correct drive letter and return it
+def get_removable_disk_drive_leter():
+    # get a list of all drives on the computer
+    result = str(subprocess.run('wmic logicaldisk get deviceid, volumename, description', capture_output=1)) 
+
+    # create a regular expression to find the correct drive letter fot the removable drive
+    removable_disk_drive_leter_regular_expression = r'(?:Removable Disk\s*)([A-Z])(?::\s*MAC-address)'
+
+    # find the drive letter
+    removable_disk_drive_leter = re.search(removable_disk_drive_leter_regular_expression, result).group(1) 
+    
+    return removable_disk_drive_leter 
+
+# generate the .bat file with the correct drive letter
+drive_letter = get_removable_disk_drive_leter()
+
+with open('f{drive_letter}:\sccm-check-for-software.bat', "w") as file:
+    file.write(f'powershell.exe -executionpolicy bypass -File "{drive_letter}:\\sccm-check-for-software\\sccm-check-for-software"') 
 
